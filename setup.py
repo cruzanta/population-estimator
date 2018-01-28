081d136334e66259399d643736f28d8b6275d37e
cruzanta <cruzanta@outlook.com>
2018-01-28 02:01:17 -0500

    Resolve packaging issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='population-estimator',
     packages=['population_estimator'],
-    version='0.3.0',
+    version='0.3.1',
     description='A TUI app for accessing annual population estimates',
     author='Anthony Cruz',
     author_email='cruzanta@outlook.com',
@@ -13,7 +13,7 @@ setup(
     entry_points={'console_scripts': [
         'population-estimator = population_estimator.tui_app:main']},
     url='https://github.com/cruzanta/population_estimator',
-    download_url='https://github.com/cruzanta/population_estimator/archive/0.3.tar.gz',
+    download_url='https://github.com/cruzanta/population_estimator/archive/0.3.1.tar.gz',
     keywords=['population', 'tui', 'curses'],
     classifiers=[],
 )
